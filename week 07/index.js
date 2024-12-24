3244ba142a53389e6b611efb486f74e56313aade
praveenlodhi99 <praveenlodhi.official@gmail.com>
2024-12-24 06:26:02 +0530

    Added Middleware, post-todo, get-todos for the Todo application

--- week 07/index.js
+++ week 07/index.js
@@ -54,6 +54,67 @@ app.post('/signin', async (req, res) => {
     }
 })
 
+function authMiddleware(req, res, next) {
+
+    const token = req.headers.authorization
+
+    if (token) {
+        jwt.verify(token, JWT_SECRET, (err, decoded) => {
+            if (err) {
+                res.status(401).json({
+                    message: 'User Unauthorized'
+                })
+            } else {
+                req.DecodedData = decoded
+                next()
+            }
+        })
+    } else {
+        res.status(401).json({
+            message: 'User Unauthorized'
+        })
+    }
+}
+
+app.post('/post-todo', authMiddleware, async (req, res) => {
+
+    const { title, description, isDone } = req.body;
+
+    const UserDetails = req.DecodedData;
+
+    // Create the new todo
+    const todo = await TodoModel.create({
+        title,
+        description,
+        UserId: UserDetails.id,
+        isDone
+    });
+
+    res.status(201).json({
+        message: 'Todo created successfully.',
+        todo
+    });
+});
+
+app.get('/get-todos', authMiddleware, async (req, res) => {
+
+    try {
+        const userId = req.DecodedData.id;
+        const todos = await TodoModel.find({ UserId: userId });
+
+        res.status(200).json({
+            message: 'Todos fetched successfully.',
+            todos
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: 'An error occurred while fetching todos.',
+            error: error.message
+        });
+    }
+});
+
 app.listen(3000, () => {
     console.log('Server Restarted \n')
 })
