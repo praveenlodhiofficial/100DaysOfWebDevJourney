f73365aaa756e227a46206a09ba229ee87acc3b8
praveenlodhi99 <praveenlodhi.official@gmail.com>
2024-12-27 20:08:46 +0530

    Learn about 'useState' hook; created a counter increamenter.

--- week 09 - React/src/App.jsx
+++ week 09 - React/src/App.jsx
@@ -1,35 +1,17 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
+const createCounter = () => {
   const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1 className='text-3xl font-bold uppercase underline'>Create Counter</h1>
+      <div className='p-5 flex gap-5 items-center'>
+        <p>Count: {count}</p>
+        <button onClick={() => setCount(count + 1)} className='border py-1 px-2 rounded bg-black text-white'>Increamenter</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
-export default App
+export default createCounter
