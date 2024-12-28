7b5dca2b90358dc8b898b5258db5f544e28f33c5
praveenlodhi99 <praveenlodhi.official@gmail.com>
2024-12-29 00:44:01 +0530

    Implemented useEffect Hook to start and stop counter

--- week 09 - React/src/App.jsx
+++ week 09 - React/src/App.jsx
@@ -1,26 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const createCounter = () => {
+    const [count, setCount] = useState(0)
+    const [play, setPlay] = useState(false)
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCount((prevCount) => prevCount + 1); // Increment count every second
-    }, 1000);
+    useEffect(() => {
+        if(play) {
+            const interval = setInterval(() => {
+                setCount(count => count + 1)
+            }, 100)
 
-    // Cleanup function to clear the interval when the component unmounts
-    return () => clearInterval(interval);
-  }, []);
+            return() => {
+                clearInterval(interval)
+            }
+        }
+    }, [play])
 
-  return (
-    <div>
-      <div>Count: {count}</div>
-      <div className="flex gap-5">
-        <button onClick={() => setCount(count + 1)}>Incrementer</button>
-        <button onClick={() => setCount(count - 1)}>Decrementer</button>
-      </div>
-    </div>
-  );
-};
+    return (
+        <div className='flex flex-col m-5 justify-center items-center'>
+            <h1 className='text-3xl font-bold uppercase underline'>Counter Function</h1>
 
-export default App;
+            <div className='p-5 flex flex-col gap-5 items-center'>
+                <p className='text-xl'>Count: {count}</p>
+
+                <div className="flex gap-5">
+
+                    {/* Increamenter Button */}
+                    <button onClick={() => setCount(count + 1)} className='border py-1 px-2 rounded bg-black text-white'>Increamenter</button>
+                    
+                    {/* Start Counter Button */}
+                    <button onClick={() => setPlay(!play)} className='border py-1 px-2 rounded bg-black text-white'>
+                        { !play ? 'Start Counter' : 'Stop Counter' }
+                    </button>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default createCounter
