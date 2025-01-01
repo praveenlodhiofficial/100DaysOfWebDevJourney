9827590c45a59ae9e1f3e3008f3d7e8d9f720a3e
praveenlodhi99 <praveenlodhi.official@gmail.com>
2025-01-02 01:07:46 +0530

    Learned abou the 'useRef' hook in the form inputs field

--- week 09 - React/src/App.jsx
+++ week 09 - React/src/App.jsx
@@ -1,50 +1,44 @@
-import { BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom";
+import React, { useRef } from 'react';
 
-function App() {
-  return (
-    <div className="border p-5 justify-center items-start text-center font-[laila]" >
+const App = () => {
+  const shouldFocus = useRef();
 
-      <BrowserRouter>
-
-        <nav className="flex gap-5 text-center justify-center font-bold">
-          <Link to="/">Allen</Link> | 
-          <Link to="/neet/online-coaching-class-11">Class 11</Link> | 
-          <Link to="/neet/online-coaching-class-12">Class 12</Link>
-        </nav>
-
-        <Routes>
-          <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
-          <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
-          <Route path="/" element={<Landing />} />
-        </Routes>
-
-      </BrowserRouter>
-
-    </div>
-  );
-}
-
-function Landing() {
-  return <div className="text-4xl p-40">Welcome to Allen</div>;
-}
-
-function Class11Program() {
-  return <div className="text-4xl p-40">NEET programs for Class 11th</div>;
-}
-
-function Class12Program() {
-  const navigate = useNavigate();
-
-  function redirectUser() {
-    navigate('/');
+  function focusOnInput() {
+    shouldFocus.current.focus(); // Focus on the first input
   }
 
   return (
-    <div className="text-4xl p-40 flex flex-col gap-5 items-center">
-      <p>NEET programs for Class 12th</p>
-      <button onClick={redirectUser} className="text-xl border px-3 py-2 text-center bg-black w-[20%] text-white rounded-md">Go Back to Home</button>
-    </div>
+    <>
+      <form
+        action="#"
+        className="border-black border flex flex-col w-80 p-2 rounded-lg m-5 gap-2 font-[laila]"
+      >
+        <input
+          type="text"
+          placeholder="Name"
+          ref={shouldFocus}
+          className="border border-dotted border-black px-2 py-1 text-center rounded"
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          className="border border-dotted border-black px-2 py-1 text-center rounded"
+        />
+        <input
+          type="text"
+          placeholder="Phone"
+          className="border border-dotted border-black px-2 py-1 text-center rounded"
+        />
+        <button
+          type="button"
+          onClick={focusOnInput}
+          className="border border-dotted border-white px-2 py-2 text-center rounded bg-black text-white text-sm"
+        >
+          Focus on First Input
+        </button>
+      </form>
+    </>
   );
-}
+};
 
 export default App;
