3d48498a1b5bdccf5c3623e60c775886683cc576
praveenlodhi99 <praveenlodhi.official@gmail.com>
2025-01-03 12:12:34 +0530

    Implemented 'usePrev' hook

--- week 09 - React/src/App.jsx
+++ week 09 - React/src/App.jsx
@@ -1,38 +1,17 @@
-import React, { useState } from 'react';
-import { useFetch } from './hooks/useFetch';
+import React, { useState } from 'react'
+import { usePrev } from './hooks/usePrev'
 
 const App = () => {
-  const [posts, setPosts] = useState(1);
-  const { finalData, loading } = useFetch(`https://jsonplaceholder.typicode.com/posts/${posts}`);
+  const [count, setCount] = useState(0)
+  const prev = usePrev(count)
 
   return (
     <>
-
-      <button onClick={() => setPosts(1)} className="border border-black rounded m-1 px-2 py-1 bg-black text-white"> Post 01 </button>
-      <button onClick={() => setPosts(2)} className="border border-black rounded m-1 px-2 py-1 bg-black text-white"> Post 02 </button>
-      <button onClick={() => setPosts(3)} className="border border-black rounded m-1 px-2 py-1 bg-black text-white"> Post 03 </button>
-      <button onClick={() => setPosts(4)} className="border border-black rounded m-1 px-2 py-1 bg-black text-white"> Post 04 </button>
-
-
-      {loading ? (
-        <div className="flex justify-center items-center h-20 text-lg font-bold text-red-500"> Loading... </div>
-      ) : (
-        <>
-          <div className="flex flex-col gap-5 border m-1 px-2 py-1 rounded border-black">
-            Post No.: {JSON.stringify(finalData.id)}
-          </div>
-
-          <div className="flex flex-col gap-5 border m-1 px-2 py-1 rounded border-black">
-            Title: {JSON.stringify(finalData.title)}
-          </div>
-
-          <div className="flex flex-col gap-5 border m-1 px-2 py-1 rounded border-black">
-            Body: {JSON.stringify(finalData.body)}
-          </div>
-        </>
-      )}
+    <div className='m-1 px-2 py-1 border border-black rounded'>The Current count is {count}</div>
+    <div className='m-1 px-2 py-1 border border-black rounded'>The Previous count was {prev}</div>
+    <button className='m-1 px-2 py-1 border border-black rounded text-white bg-black' onClick={() => setCount(count => count + 1)}>Increamenter</button>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
