81a31537e7a992c649b6e95613b3837b152b89ba
praveenlodhi99 <praveenlodhi.official@gmail.com>
2025-01-09 19:13:46 +0530

    Created a function 'greet()' function in typescript.

---
+++ week 14 - Typescript/index.js
@@ -0,0 +1,8 @@
+function greet(name) {
+    console.log("Hello, ".concat(name, "."));
+}
+function print() {
+    var name = 'Praveen Lodhi';
+    greet(name);
+}
+print();
---
+++ week 14 - Typescript/index.ts
@@ -0,0 +1,10 @@
+function greet ( name: string ) {
+    console.log(`Hello, ${name}.`)
+}
+
+function print() {
+    const name: string = 'Praveen Lodhi'
+    greet(name)
+}
+
+print();
