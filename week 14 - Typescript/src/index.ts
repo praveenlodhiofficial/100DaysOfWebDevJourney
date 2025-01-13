f2e746d4b34d1cea47681b099f4113421eaac45a
praveenlodhi99 <praveenlodhi.official@gmail.com>
2025-01-13 22:09:36 +0530

    Setting up project for second brain app

--- week 14 - Typescript/src/index.ts
+++ week 14 - Typescript/src/index.ts
@@ -1,13 +1,13 @@
-enum ResponseStatus {
-    Success = 200,
-    NotFound = 404,
-    Error = 500
-}
+// enum ResponseStatus {
+//     Success = 200,
+//     NotFound = 404,
+//     Error = 500
+// }
 
-app.get("/', (req, res) => {
-    if (!req.query.userId) {
-			res.status(ResponseStatus.Error).json({})
-    }
-    // and so on...
-		res.status(ResponseStatus.Success).json({});
-})
+// app.get("/', (req, res) => {
+//     if (!req.query.userId) {
+// 			res.status(ResponseStatus.Error).json({})
+//     }
+//     // and so on...
+// 		res.status(ResponseStatus.Success).json({});
+// })
