fe1468341571060cc93134c349d21c21a45a39b1
praveenlodhi99 <praveenlodhi.official@gmail.com>
2025-01-15 21:02:27 +0530

    SBA - Setting up 'config.ts', '.env' file and some adjustment in 'tsconfig.json'

---
+++ week 15 - Creating a Second Brain Application using Typescript/src/config.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+if (!process.env.PORT || !process.env.MONGO_URI || !process.env.USER_JWT_SECRET || !process.env.ADMIN_JWT_SECRET) {
+  throw new Error('Missing required environment variables.');
+}
+
+const config = {
+  PORT: process.env.PORT,
+  MONGO_URI: process.env.MONGO_URI,
+  USER_JWT_SECRET: process.env.USER_JWT_SECRET,
+  ADMIN_JWT_SECRET: process.env.ADMIN_JWT_SECRET,
+};
+
+export { config };
