3b3f1cd06d89ddcd25c795f5adfdf6c993cfa59c
praveenlodhi99 <praveenlodhi.official@gmail.com>
2025-01-25 22:49:16 +0530

    Best to write SQL query, to avoid SQL ingestion.

--- week 17 - Postgres and SQL databases/src/routes.ts
+++ week 17 - Postgres and SQL databases/src/routes.ts
@@ -1,12 +1,27 @@
-import express from 'express'
-const appRouter = express.Router()
+import express from 'express';
+import { pgClient } from './config/dbClient';
+
+const appRouter = express.Router();
 
 // ---------------------------------------->
-
 appRouter.post('/signup', async (req, res) => {
-    
-})
+    try {
+        const { username, email, password } = req.body;
 
+        const signupQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3)`;
+        await pgClient.query (signupQuery, [username, email, password]);
+
+        res.json({
+            message: 'You have Signed Up'
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            error: 'An error occurred while signing up.'
+        });
+    }
+});
 // ---------------------------------------->
 
-export { appRouter }
+export { appRouter };
