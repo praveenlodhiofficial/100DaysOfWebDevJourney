9a47ee84ec1f38b998139696d9912dbc9fdadec8
praveenlodhi99 <praveenlodhi.official@gmail.com>
2025-01-27 04:09:17 +0530

    Created pages for authentication 'signin' & 'signup'

---
+++ week-19-nextjs/src/app/signup/page.tsx
@@ -0,0 +1,33 @@
+export default function Signin() {
+    return (
+        <div className="flex flex-col gap-5 p-5">
+
+            <h1 className="text-6xl uppercase">Signup Page</h1>
+
+
+            <div className="flex gap-5">
+                <input
+                    type="text"
+                    name=""
+                    id=""
+                    placeholder="username"
+                    className="p-1 bg-transparent border rounded-md border-gray-500 text-center"
+                />
+                <input
+                    type="text"
+                    name=""
+                    id=""
+                    placeholder="password"
+                    className="p-1 bg-transparent border rounded-md border-gray-500 text-center"
+                />
+
+                <button
+                    className="py-1 px-3 bg-white text-black border rounded-md "
+                >
+                    Submit
+                </button>
+            </div>
+
+        </div>
+    )
+}
