26dbcc61c8f2a4cbeb73da47970a691e207bbebd
praveenlodhi99 <praveenlodhi.official@gmail.com>
2025-02-26 23:57:20 +0530

    refactor (apps/ws-backend) - some changes in the web socket code

--- week 21 - Monorepos and turborepo/excalidraw/apps/ws-backend/src/index.ts
+++ week 21 - Monorepos and turborepo/excalidraw/apps/ws-backend/src/index.ts
@@ -1,45 +1,40 @@
-import jwt from 'jsonwebtoken';
+import jwt, { decode } from 'jsonwebtoken';
 import { WebSocketServer } from 'ws';
 import { JWT_SECRET } from '@repo/backend-common/config';
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', (ws, request) => {
-    try {
-        // Parse query parameters from the URL
-        const url = request.url?.split('?')[1] || ''
-        const queryParams = new URLSearchParams(url);
-        const token = queryParams.get('token') || '';
-
-        // Verify the JWT
-        let decoded;
-        try {
-            decoded = jwt.verify(token, JWT_SECRET);
-        } catch (err) {
-            ws.close(1008, 'Invalid token'); // Close with policy violation (1008)
-            return;
-        }
-
-        // Validate decoded token
-        if (typeof decoded !== 'object' || !('userId' in decoded)) {
-            ws.close(1008, 'Invalid token payload');
-            return;
-        }
-
-        const userId = decoded.userId;
-
-        // Handle incoming messages
-        ws.on('message', (message) => {
-            console.log(`Received message from user ${userId}: ${message}`);
-            ws.send('pong');
-        });
-
-        console.log(`WebSocket connection established for user ${userId}`);
+function cheakUser(token: string): string | null {
+    const decoded = jwt.verify(token, JWT_SECRET);
     
-    } catch (error) {
-    
-        console.error('WebSocket error:', error);
-        ws.close(1011, 'ws-index : Internal server error'); 
-    
+    if(typeof decoded === 'string') {
+        return null
     }
-});
+
+    if(!decoded || !decoded.userId) {
+        return null
+    }
+    return decoded.userId
+}
+
+wss.on('connection', (ws, request) => {
+    const url = request.url;
+
+    if(!url) [
+        ws.close(1003, 'No url provided'),
+        return;
+    ]
+
+    const queryParams = new URLSearchParams(url?.split('?'[1]))
+    const token = queryParams.get('token') || '';
+    const userId = cheakUser(token)
+
+    if(!userId) {
+        ws.close(1003, 'User not authenticated'),
+        return;
+    }
+
+    ws.on('message', (data) => {
+        ws.send(data);
+    }
+})
