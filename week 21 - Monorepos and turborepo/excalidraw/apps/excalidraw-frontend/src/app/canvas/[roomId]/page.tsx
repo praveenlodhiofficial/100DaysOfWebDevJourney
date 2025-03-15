1538b297e4bf17bf430df0307d58aac70d0f0b0b
praveenlodhi99 <praveenlodhi.official@gmail.com>
2025-03-16 01:23:51 +0530

    feat(excalidraw-frontend): add basic canvas with rectangle drawing functionality
    - Created a `Canvas` component inside `app/canvas`.
    - Enabled rectangle drawing on the canvas using `mousedown`, `mousemove`, and `mouseup` events.
    - Implemented a mechanism to dynamically update the rectangle dimensions as the user drags the mouse.
    - Added event listeners to handle drawing and context updates.
    - Used a `useRef` hook to manage the canvas element and ensure reactivity with `useEffect`.

---
+++ week 21 - Monorepos and turborepo/excalidraw/apps/excalidraw-frontend/src/app/canvas/[roomId]/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
+export default function Canvas() {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+        if (canvasRef.current) {
+            const canvas = canvasRef.current;
+            const ctx = canvas.getContext("2d");
+
+            if (!ctx) {
+                console.error("Failed to get 2d context");
+                return;
+            }
+
+            let clicked = false;
+            let startX = 0;
+            let startY = 0;
+
+            canvas.addEventListener("mousedown", (e) => {
+                clicked = true;
+                startX = e.clientX;
+                startY = e.clientY;
+            });
+
+            canvas.addEventListener("mouseup", (e) => {
+                clicked = false;
+                console.log(e.clientX, e.clientY);
+            });
+
+            canvas.addEventListener("mousemove", (e) => {
+                if (clicked) {
+                    const width = e.clientX - startX;
+                    const height = e.clientY - startY;
+
+                    ctx.clearRect(0, 0, canvas.width, canvas.height);
+                    ctx.strokeRect(startX, startY, width, height);
+                }
+            });
+        }
+    }, []);
+
+    return (
+        <div>
+            <canvas
+                ref={canvasRef}
+                width={1000}
+                height={1000}
+            >
+            </canvas>
+        </div>
+    )
+}
