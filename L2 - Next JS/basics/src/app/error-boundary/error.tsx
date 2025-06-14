cef4a97a757fd291bd49abbe1b088fadf6407566
Praveen Lodhi <praveenlodhi.official@gmail.com>
2025-06-14 16:27:43 +0530

    L2 - Learned about useRouter hook & Error Boundary

---
+++ L2 - Next JS/basics/src/app/error-boundary/error.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useEffect } from "react"
+
+export default function ErrorFallbackUI({ error, reset }: {
+    error: Error,
+    reset: () => void
+}) {
+
+    useEffect(() => {
+
+    }, [error])
+    
+    return (
+        <div className="flex justify-center items-center h-screen uppercase text-3xl font-medium text-red-500">
+            {error?.message || 'An Error Occured'}
+
+        </div>
+    )
+}
--- L2 - Next JS/basics/src/app/page.tsx
+++ L2 - Next JS/basics/src/app/page.tsx
@@ -1,9 +1,34 @@
+"use client";
+
 import Container from "@/components/ui/container";
+import { useRouter } from "next/navigation";
+import { useRef, useState } from "react";
 
 export default function Home() {
+  const [show, setShow] = useState<boolean>(false);
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    router.push("/projects")
+  }
+
   return (
-    <Container>
-      <div>Hello World</div>
+    <Container classname="flex gap-10 flex-col justify-center items-center">
+      <button
+        className="bg-white/5 px-8 py-2 rounded-lg w-fit cursor-pointer"
+        onClick={() => setShow(!show)}
+      > Click
+      </button>
+
+      <div className={`${show ? "block" : "hidden"}`} > Content Visible </div>
+
+      <button
+        className="bg-white/5 px-8 py-2 rounded-lg w-fit cursor-pointer"
+        onClick={() => handleNavigation()}
+      > Go to Project Page
+      </button>
+
     </Container>
   );
 }
+
