be9aa0ccace261f5caeef8bd2ae3eb05354c6326
Praveen Lodhi <praveenlodhi.official@gmail.com>
2025-08-05 23:31:48 +0530

    week 27 - monorepo - web app successfully fetching data from websocket

--- week 27 - Docker/week-27-docker-compose-monorepo/apps/http-server/src/index.ts
+++ week 27 - Docker/week-27-docker-compose-monorepo/apps/http-server/src/index.ts
@@ -48,5 +48,5 @@ app.get("/users", async (req, res) => {
 })
 
 app.listen(3001, () => {
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port 3001");
 })
--- week 27 - Docker/week-27-docker-compose-monorepo/apps/web/app/layout.tsx
+++ week 27 - Docker/week-27-docker-compose-monorepo/apps/web/app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
@@ -23,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={roboto.className}> 
         {children}
       </body>
     </html>
