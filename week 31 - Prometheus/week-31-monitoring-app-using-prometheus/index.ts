1d166eef26e5c30c68a4898ada8a18f762683fcb
Praveen Lodhi <praveenlodhi.official@gmail.com>
2025-09-07 02:14:36 +0530

    week 31 - created a middleware and '/metrics' route to expose the metrics data

--- week 31 - Prometheus/week-31-monitoring-app-using-prometheus/index.ts
+++ week 31 - Prometheus/week-31-monitoring-app-using-prometheus/index.ts
@@ -1,5 +1,34 @@
 import express from 'express'
-import { middleware } from './middleware';
+import type { Request, Response, NextFunction } from 'express';
+import promClient from 'prom-client';
+
+// Counter to track the number of requests
+const requestCounter = new promClient.Counter({
+    name: 'request_count',
+    help: 'Total number of requests',
+    labelNames: ['method', 'route', 'status']
+});
+
+// Middleware to calculate performance metrics
+function middleware(req: Request, res: Response, next: NextFunction) {
+    const startTime = Date.now();
+
+    res.on('finish', () => {
+        const endTime = Date.now();
+        const duration = endTime - startTime;
+
+        requestCounter.inc({
+            method: req.method,
+            route: req.url,
+            status: res.statusCode
+        });
+
+        console.log(`----------------------------------------------------------------`);
+        console.log(`[middleware] ${req.method} | route: ${req.url} | duration: ${duration}ms | status: ${res.statusCode}`);
+    });
+
+    next();
+}
 
 const app = express();
 app.use(middleware);
@@ -17,6 +46,13 @@ app.get("/users", (req, res) => {
     res.json({
         message: "Users are working"
     })
+})
+
+app.get("/metrics", async (req, res) => {
+    const metrics = await promClient.register.metrics();
+    console.log(promClient.register.contentType);
+    res.setHeader('Content-Type', promClient.register.contentType);
+    res.send(metrics);
 })
 
 app.listen(3000, () => {
